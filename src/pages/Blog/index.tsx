8a89bd3ffe9c7e21011dd01b7b7d22de7db6741f
Rafael Mac Menz <rmcmenz@gmail.com>
2022-11-07 08:45:39 -0300

    axios

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -7,37 +7,64 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { BlogContainer, InputContainer, Posts, Profile } from './styles'
 import { Post } from './components/Post'
+import { api } from '../../lib/api'
+import { useEffect, useState } from 'react'
+
+interface User {
+  login: string
+  name: string
+  bio: string
+  company: string
+  htmlUrl: string
+  followers: number
+}
 
 export function Blog() {
+  const [user, setUser] = useState<User>({
+    login: '',
+    name: '',
+    bio: '',
+    company: '',
+    htmlUrl: '',
+    followers: 0,
+  })
+
+  async function LoadUser() {
+    const response = await api.get<User>('users/m4cm3nz')
+    setUser(response.data)
+  }
+
+  useEffect(() => {
+    LoadUser()
+  }, [])
+
   return (
     <BlogContainer>
       <Profile>
         <img src="https://github.com/m4cm3nz.png" alt=""></img>
         <div>
           <header>
-            <h5>Rafael Mac Menz</h5>
+            <h5>{user.name}</h5>
             <a href="#">
               GITHUB
               <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
             </a>
           </header>
-          <p>
-            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-            volutpat pulvinar vel mass.
-          </p>
+          <p>{user.bio}</p>
           <nav>
-            <a href="#">
+            <a href={user.htmlUrl}>
               <FontAwesomeIcon icon={faGithub} />
-              m4cm3nz
+              {user.login}
             </a>
-            <a href="#">
-              <FontAwesomeIcon icon={faBuilding} />
-              m4cm3nz.it
-            </a>
+            {user.company && (
+              <a href="#">
+                <FontAwesomeIcon icon={faBuilding} />
+                {user.company}
+              </a>
+            )}
             <a href="#">
               <FontAwesomeIcon icon={faUserGroup} />
-              11 seguidores
+              {user.followers} seguidores
             </a>
           </nav>
         </div>
